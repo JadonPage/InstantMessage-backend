15afe84988a8392ef86a2f2b0b9177e4943d8b4a
jadon <jadontpage@outlook.com>
2024-01-16 07:57:02 -0600

    added new url to allowed origins

--- core/settings.py
+++ core/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'corsheaders',
 ]
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
+CORS_ALLOWED_ORIGINS = ['https://jadonpage.com']
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
